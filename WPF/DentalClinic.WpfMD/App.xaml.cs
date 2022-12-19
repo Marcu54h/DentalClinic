62e2a500a9a25ba86ac24bc9b7eaf4435b96d68f
Adam <adam.marzec@gmail.com>
2022-12-19 21:41:32 +0100

    Added DependencyInjection.

--- WPF/DentalClinic.WpfMD/App.xaml.cs
+++ WPF/DentalClinic.WpfMD/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace DentalClinic.WpfMD
@@ -13,5 +9,33 @@ namespace DentalClinic.WpfMD
     /// </summary>
     public partial class App : Application
     {
+        public static IHost? AppHost { get; private set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                }).Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+
+            var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+
+            startupForm.Show();
+
+            base.OnStartup(e);
+
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost!.StopAsync();
+            base.OnExit(e);
+        }
     }
 }
