e1bf19f550f801fa2c50c4205f5f3f8095b90e07
Adam Marzec <adam.marzec@gmail.com>
2022-12-21 12:09:06 +0100

    Added IAsyncCommand with implementation. LeftDrawer is functional.

---
+++ WPF/DentalClinic.WpfMD/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using DentalClinic.WpfMD.Abstraction;
+using System.Threading.Tasks;
+
+namespace DentalClinic.WpfMD.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private bool isLeftDrawerOpen = false;
+        public object CurrentView { get; set; }
+
+        public bool IsLeftDrawerOpen
+        {
+            get => isLeftDrawerOpen;
+            set
+            {
+                isLeftDrawerOpen = value;
+                Changed(nameof(IsLeftDrawerOpen));
+            }
+        }
+
+        public MainViewModel()
+        {
+            CurrentView = new PatientsViewModel();
+
+            CommandToOpenLeftDrawer = new AsyncCommand<bool>(
+                async (choice) => await Task.Run(() => IsLeftDrawerOpen = choice));
+        }
+
+        public IAsyncCommand<bool> CommandToOpenLeftDrawer { get; set; } 
+    }
+}
---
+++ WPF/DentalClinic.WpfMD/ViewModels/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace DentalClinic.WpfMD.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void Changed(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
