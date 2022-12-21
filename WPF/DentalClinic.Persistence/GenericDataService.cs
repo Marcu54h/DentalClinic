a503b4a7e0a0f078249af410e8274466a7b43d3b
Adam Marzec <adam.marzec@gmail.com>
2022-12-21 19:12:16 +0100

    Persistence project added to WpfMD.

---
+++ WPF/DentalClinic.Persistence/GenericDataService.cs
@@ -0,0 +1,44 @@
+﻿using DentalClinic.WpfMD.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDataSource;
+
+namespace DentalClinic.Persistence
+{
+    internal class GenericDataService<T> : IDataService<T>
+    {
+        private readonly ClinicContextFactory clinicContextFactory;
+        public GenericDataService(ClinicContextFactory)
+        {
+
+        }
+
+        public Task<T> Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF/DentalClinic.WpfMD/Abstraction/IErrorHandler.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DentalClinic.WpfMD.Abstraction
+{
+    public interface IErrorHandler
+    {
+        void HandleError(Exception ex);
+    }
+}
