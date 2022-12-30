35f97da6fc53615ee2d4616c9c562a89f5b7970b
Adam <adam.marzec@gmail.com>
2022-12-30 12:01:39 +0100

    Add classes and styles for Schedule control.

---
+++ WPF/DentalClinic.WpfMD/Controls/CalendarView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace DentalClinic.WpfMD.Controls
+{
+    public class CalendarView : ViewBase
+    {
+        private CalendarViewPeriodCollection _periods = new();
+
+        public CalendarViewPeriodCollection Periods
+        {
+            get => _periods;
+        }
+
+        public BindingBase ItemBeginBinding { get; set; }
+        public BindingBase ItemEndBinding { get; set; }
+
+        protected override object DefaultStyleKey =>
+            new ComponentResourceKey(GetType(), "DefaultStyleKey");
+
+        protected override object ItemContainerDefaultStyleKey =>
+            new ComponentResourceKey(GetType(), "ItemContainerDefaultStyleKey");
+    }
+}
---
+++ WPF/DentalClinic.WpfMD/Controls/CalendarViewPeriod.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DentalClinic.WpfMD.Controls
+{
+    public class CalendarViewPeriod : DependencyObject
+    {
+        public static readonly DependencyProperty BeginProperty =
+            DependencyProperty.Register("Begin", typeof(DateTime),
+                typeof(CalendarViewPeriod));
+
+        public static readonly DependencyProperty EndProperty =
+            DependencyProperty.Register("End", typeof(DateTime),
+                typeof(CalendarViewPeriod));
+
+        public static readonly DependencyProperty HeaderProperty = 
+            DependencyProperty.Register("Header", typeof(object),
+                typeof(CalendarViewPeriod));
+
+        public DateTime Begin
+        {
+            get => (DateTime)GetValue(BeginProperty);
+            set => SetValue(BeginProperty, value);
+        }
+
+        public DateTime End
+        {
+            get => (DateTime)GetValue(EndProperty);
+            set => SetValue(EndProperty, value);
+        }
+
+        public object Header
+        {
+            get => GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+    }
+}
